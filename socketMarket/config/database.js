f35d5e8fe27f5630c17cc5230c845f502fd7e086
Andre Aru <aa.andrearu@gmail.com>
2019-01-05 23:34:57 +0200

    remove socketMarket

--- socketMarket/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	database: 'mongodb://localhost/socketMarket',
-	secret: 'yoursecret'
-}
--- socketMarket/routes/crypto.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//crypto home
-router.get('/', (req, res) => {
-	res.render('crypto');
-});
-
-module.exports = router;
